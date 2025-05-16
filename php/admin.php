45bf3f5b3879f02cec3ccc8085d4b111ecab9e5c
Hayatuden <orhanbeym123@gmail.com>
2025-05-16 10:06:49 +0300

    admin page

--- php/admin.php
+++ php/admin.php
@@ -38,3 +38,45 @@ if (!isset($_SESSION['adminUname']) || $_SESSION['Username'] !== 'hayu') {
         header("Location: ../src/index.php");
         exit();
     }
+
+    if (isset($_POST['submit'])) {
+        $courseName = $_POST['courseName'];
+
+        if (isset($_FILES['courseImage']) && $_FILES['courseImage']['error'] === UPLOAD_ERR_OK) {
+            // Get the binary content of the uploaded image
+            $image = file_get_contents($_FILES['courseImage']['tmp_name']);
+
+
+            $escapedImage = mysqli_real_escape_string($conn, $image);
+
+            // Construct the SQL query
+
+            $sqlCheck = "SELECT * FROM course WHERE course_name = '$courseName'";
+            $checkResult = mysqli_query($conn, $sqlCheck);
+            $row = mysqli_num_rows($checkResult);
+            if ($row !== 0) {
+                echo "<script>alert('Course already exists!');</script>";
+                header("Location: ./admin.php");
+            } else {
+                $sql = "INSERT INTO course (course_name, images) VALUES ('$courseName', '$escapedImage')";
+
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    echo "<script>alert('Course added successfully!');</script>";
+                } else {
+                    echo "<script>alert('Error adding course: " . mysqli_error($conn) . "');</script>";
+                }
+            }
+        } else {
+            echo "<script>alert('Please upload a valid image.');</script>";
+        }
+    }
+
+    if (isset($_POST['Delete'])) {
+        $id = $_POST['id'];
+        $query = "DELETE FROM course WHERE id = '$id'";
+        $result = mysqli_query($conn, $query);
+    }
+}
+
