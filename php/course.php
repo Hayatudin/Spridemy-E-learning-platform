02edb2b8dde86fb702f38be8ff0cc59c18ec45fb
Hayatuden <orhanbeym123@gmail.com>
2025-05-16 10:37:50 +0300

    course content

--- php/course.php
+++ php/course.php
@@ -111,3 +111,61 @@ if (isset($_SESSION['Username'])) {
           <div class="bg-orange-600 rounded-full w-7 h-7 text-center">5</div>
           <p class="sm:hidden lg:block" id="outlineList">Advanced JavaScript</p>
         </div>
+        <div
+          class="flex container gap-2 cursor-pointer hover:text-gray-300 transition-300" id="outlineList">
+          <div class="bg-orange-600 rounded-full w-7 h-7 text-center">6</div>
+          <p class="sm:hidden lg:block" id="outlineList">Frontend Frameworks</p>
+        </div>
+        <div
+          class="flex container gap-2 cursor-pointer hover:text-gray-300 transition-300" id="outlineList">
+          <div class="bg-orange-600 rounded-full w-7 h-7 px-3 text-center">
+            7
+          </div>
+          <p class="sm:hidden lg:block" id="outlineList">Version Control and Deployment</p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="container flex items-center flex-col gap-4 flex-1 overflow-y-auto">
+      <div class="container bg-orange-600 rounded-sm px-3 py-1 w-full mt-10">
+        <h2 class="lg:text-xl sm:text-sm text-white font-bold" id="heads">
+
+        </h2>
+      </div>
+      <div
+        class="lg:w-[80%] sm:w-[90%] py-5 border-b-2 flex flex-col gap-2 sm:text-sm lg:text-lg">
+        <video
+          id="video"
+          src="../VIDEO/Frontend-web-development-a-complete-overview-2021.mp4"
+          controls></video>
+        <div class="flex flex-col gap-2">
+          <h2 class="" id="Hdesc">
+
+          </h2>
+          <h2 class="text-orange-600 font-bold lg:text-xl sm:text-sm">
+            Main concepts
+          </h2>
+          <p class="font-bold lg:text-xl sm:text-sm" id="Ldesc">
+
+          </p>
+
+          <p class="container" id="L1desc">
+
+
+          </p>
+          <p class="font-bold lg:text-xl sm:text-sm" id="L2Head"></p>
+
+          <p class="container" id="L2desc">
+
+
+          </p>
+          <p class="font-bold lg:text-xl sm:text-sm" id="L3Head"></p>
+
+          </p>
+
+          <p class="container" id="L3desc">
+
+
+          </p>
+        </div>
+      </div>
