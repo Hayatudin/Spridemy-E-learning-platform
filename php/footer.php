eb858125444043f77cea52e9f0d1d99d936a690d
Hayatuden <orhanbeym123@gmail.com>
2025-05-17 10:53:37 +0300

    footer

---
+++ php/footer.php
@@ -0,0 +1,76 @@
+<!-------------------- footer --------------->
+<footer class=" w-full bg-gray-800 flex flex-col items-center bottom-0 ">
+    <div
+        class=" flex sm:flex-col lg:items-start sm:items-center sm:justify-center lg:flex-row lg:justify-evenly gap-3 mt-3 w-full">
+        <div class="flex flex-col items-center gap-2 lg:w-[30%]">
+            <h2 class="text-2xl font-bold text-yellow-500 text-center">
+                Lessonic.
+            </h2>
+            <h3 class="text-sm text-gray-400 desc">
+                Learn new skills, enhance your knowledge, and achieve your goals
+                with our expert-led online courses.
+            </h3>
+            <div class="flex items-center gap-5">
+                <a href="#">
+                    <i
+                        class="ri-github-fill text-2xl hover:text-yellow-500 duration-300 cursor-pointer"></i>
+                </a>
+                <a href="#">
+                    <i
+                        class="ri-tiktok-fill text-2xl hover:text-yellow-500 duration-300 cursor-pointer"></i>
+                </a>
+                <a href="#">
+                    <i
+                        class="ri-instagram-fill text-2xl hover:text-yellow-500 duration-300 cursor-pointer"></i>
+                </a>
+                <a href="#">
+                    <i
+                        class="ri-youtube-fill text-2xl hover:text-yellow-500 cursor-pointer"></i>
+                </a>
+            </div>
+        </div>
+
+        <div>
+            <h2 class="text-xl font-bold text-yellow-500">Exlore</h2>
+            <a href="./home.php">
+                <p class="links hover:opacity-75">Home</p>
+            </a>
+            <a href="./roadmap.php">
+                <p class="links hover:opacity-75">Roadmap</p>
+            </a>
+            <a href="./mylearning.php">
+                <p class="links hover:opacity-75">My learning</p>
+            </a>
+        </div>
+        <div>
+            <h2 class="text-xl font-bold text-yellow-500">Support</h2>
+            <a href="#">
+                <p class="links hover:opacity-75">How It Works</p>
+            </a>
+            <a href="#">
+                <p class="links hover:opacity-75">Help Center</p>
+            </a>
+            <a href="#">
+                <p class="links hover:opacity-75">FAQs</p>
+            </a>
+            <a href="#">
+                <p class="links hover:opacity-75">Contact Us</p>
+            </a>
+        </div>
+        <div>
+            <h2 class="text-xl font-bold text-yellow-500">Legal</h2>
+            <a href="#">
+                <p class="links hover:opacity-75">Terms of Service</p>
+            </a>
+            <a href="#">
+                <p class="links hover:opacity-75">Privacy Policy</p>
+            </a>
+            <a href="#">
+                <p class="links hover:opacity-75">Cookie Policy</p>
+            </a>
+        </div>
+    </div>
+    <p class="text-sm text-gray-400 pt-3 pb-3 text-center">
+        2025 &copy; All rights reserved. created by hy creative
+    </p>
+</footer>
