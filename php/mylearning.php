f0af6707ae72748d6bcdc8e4350d58a05991f797
Hayatuden <orhanbeym123@gmail.com>
2025-05-17 10:58:41 +0300

    header

--- php/mylearning.php
+++ php/mylearning.php
@@ -68,3 +68,41 @@ if (isset($_POST['continue'])) {
 }
 
 ?>
+
+<div
+  class="container bg-gray-800 rounded-lg pt-5 pb-5 profile flex w-[80%] lg:justify-between mt-24 mb-7 !important" style="margin: 40px 0 10px 0;">
+
+  <div class="flex gap-2 lg:gap-3" style="background-image: url('../image/code.png'); background-size: contain; background-position: right; background-repeat:no-repeat;">
+
+    <a href="./profile.php">
+      <div class="text-xl rounded-full  text-white flex items-center justify-center font-semibold  " style="background-color: green; width: 35px; height: 35px;">
+        <p class=" pb-[3px] capitalize"><?php echo $firstLetter; ?></p>
+      </div>
+    </a>
+    <div class="">
+      <h2 class="font-bold lg:text-3xl sm:text-xl">
+        <?php
+        echo $user_row['FullName'];
+        ?>
+      </h2>
+      <p class=" " style="font-size: 8px">
+        <?php echo $user_row['proficiency'] ?>
+      </p>
+      <div class="flex items-center text-orange-600">
+        <i class="ri-cellphone-fill"></i>
+        <p class="text-green-600 font-bold text-sm lg:text-lg">
+          <?php echo $user_row['pNumber'] ?>
+        </p>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <p class="text-sm">4.9</p>
+        <img src="../image/stars.png" class="w-20" draggable="false" />
+      </div>
+    </div>
+  </div>
+  <a href="./profile.php">
+    <i class="ri-file-edit-line cursor-pointer hover:text-orange-600 right-0 text-xl"></i>
+  </a>
+</div>
+</header>
