33789006ef83296376a677b55e04a228bd869974
Hayatuden <orhanbeym123@gmail.com>
2025-05-18 22:19:23 +0300

    roadmap

--- php/roadmap.php
+++ php/roadmap.php
@@ -99,3 +99,52 @@
             </div>
           </div>
         </div>
+        <!-- Timeline Item 7 -->
+        <div
+          class="flex flex-col items-center lg:flex-row lg:items-start lg:space-x-6 lg:mb-10">
+          <div class="hidden lg:block lg:w-1/2"></div>
+          <div class="lg:w-1/2">
+            <div class="p-6 rounded-md shadow-lg">
+              <img
+                src="../lists/responsive-01.png"
+                draggable="false"
+                class="w-full" />
+            </div>
+          </div>
+        </div>
+
+        <!-- Timeline Item 8 -->
+        <div
+          class="flex flex-col-reverse items-center lg:flex-row-reverse lg:items-start lg:space-x-6 lg:space-x-reverse lg:mb-10">
+          <div class="hidden lg:block lg:w-1/2"></div>
+          <div class="lg:w-1/2">
+            <div class="p-6 rounded-md shadow-lg">
+              <img
+                src="../lists/project-01.png"
+                draggable="false"
+                class="w-full" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container text-center courses pb-5">
+    <h2 class="sm:text-sm lg:text-2xl lg:px-5 pt-5 pb-5">
+      Our website provides a clear, step-by-step roadmap from learning the
+      basics to mastering advanced techniques.
+    </h2>
+    <input
+      type="submit"
+      value="Explore careers"
+      class="bg-orange-600 px-5 py-2 rounded-sm mt-2 sm:text-sm sm:py-3 sm:py-2 mb-5" />
+  </div>
+</main>
+
+<?php include "./footer.php"; ?>
+
+<script src="../road.js"></script>
+<script src="../index.js"></script>
+</body>
+
+</html>
