a9cc19a53e2b44189caa6c05dc85d988f0c0722c
Hayatuden <orhanbeym123@gmail.com>
2025-05-18 22:18:02 +0300

    profile

--- php/profile.php
+++ php/profile.php
@@ -38,3 +38,47 @@ if (isset($_POST['logout'])) {
     <h2 class="font-bold text-2xl lg:text-3xl">Profile & settings</h2>
 
   </div>
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <div class="flex flex-col gap-2">
+      <div class="flex sm:flex-col lg:flex-row gap-4 ">
+        <div class="flex flex-col lg:w-1/2 sm:w-full gap-1">
+          <label for="Fname">Full Name</label>
+          <input type="text" class="px-3 py-1 bg-slate-800 rounded-sm text-sm w-2/3 sm:w-full" placeholder="Full name" name="Fullname">
+        </div>
+        <div class="flex flex-col lg:w-1/2 sm:w-full gap-1">
+          <label for="Email">Email</label>
+          <input type="Email" class="px-3 py-1 bg-slate-800 rounded-sm text-sm" placeholder="Email" name="Email">
+        </div>
+
+      </div>
+      <div class="flex sm:flex-col lg:flex-row gap-4 ">
+
+        <div class="flex flex-col lg:w-1/2 sm:w-full gap-1">
+          <label for="Phone">Phone number</label>
+          <input type="text" class="px-3 py-1 bg-slate-800 rounded-sm text-sm" placeholder="Phone number" name="Phone">
+        </div>
+
+        <div class="flex flex-col lg:w-1/2 sm:w-full gap-1">
+          <label for="Profeincy">Proficiency</label>
+          <input type="text" class="px-3 py-1 bg-slate-800 rounded-sm text-sm" placeholder="Proficiency" name="Proficiency">
+        </div>
+      </div>
+
+      <div class="flex justify-between">
+        <input type="submit" name="submit" id="submit" value="update" class="bg-yellow-500 px-3 py-1 rounded-sm mt-6 w-28">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+          <input type="submit" name="logout" value="Logout" class="bg-red-500 px-3 py-1 rounded-sm">
+        </form>
+      </div>
+    </div>
+  </form>
+</div>
+
+<?php include "./footer.php"; ?>
+
+<script src="../index.js"></script>
+<script src="../road.js"></script>
+
+</body>
+
+</html>
